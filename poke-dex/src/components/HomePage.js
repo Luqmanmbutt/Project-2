3aaf350d5af049b0846281b3e262c5ffc2e8567e
Luqman Butt <luqmanmbutt1@gmail.com>
2023-02-01 15:38:45 +0000

    added homepage, to put all cards inside one container.

---
+++ poke-dex/src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import FirePokemon from './FirePokemon'
+import GrassPokemon from './GrassPokemon'
+import WaterPokemon from './WaterPokemon'
+
+const HomePage = () => {
+
+  return (
+    <>
+      <WaterPokemon />
+      <FirePokemon />
+      <GrassPokemon />
+    </>
+  )
+
+}
+
+export default HomePage;
