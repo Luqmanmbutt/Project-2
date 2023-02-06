943710783757300d5fdc91e444d9b2e7edec9c2a
Luqman Butt <luqmanmbutt1@gmail.com>
2023-02-06 17:52:37 +0000

    more changes to css clean up on containers added a title

--- poke-dex/src/components/FirePokemon.js
+++ poke-dex/src/components/FirePokemon.js
@@ -4,11 +4,11 @@ import axios from 'axios'
 
 const FirePokemon = () => {
 
-  
+
   let pokemon = 'charmander'
   const [data, setData] = useState(null)
   const [evolution, setEvolution] = useState('fire-pokemon-img-1')
-   
+
 
 
   const getData = async () => {
@@ -21,8 +21,8 @@ const FirePokemon = () => {
     getData()
   }, [])
 
-    console.log('data>.', data)
-  
+  console.log('data>.', data)
+
   const evoOne = () => {
     pokemon = 'charmander'
     getData()
@@ -30,7 +30,7 @@ const FirePokemon = () => {
     console.log('pokemon>', pokemon)
     console.log('data next gen>.', data)
   }
- 
+
   const evoTwo = () => {
     pokemon = 'charmeleon'
     getData()
@@ -45,48 +45,58 @@ const FirePokemon = () => {
     console.log('pokemon>', pokemon)
     console.log('data next gen>.', data)
   }
- 
+
 
   return (
-      <div className='card-container'>
+    <div className='card-container'>
+      <div className='buttons'>
         <button className='pokeball' onClick={evoOne}></button>
         <button className='greatball' onClick={evoTwo}> </button>
         <button className='ultraball' onClick={evoThree}></button>
-      <div className='card-holder'></div>
-      <div className='pokemon-card'></div>
-      <div className={evolution}></div>
-      <div className='attributes'></div>
-      <div className='stats'>
-       {data?.data.map(info => {
-          return (
-            <>
-              <p 
-               key={info.data}>
-                {info.data}
-                
-              </p>
-              <p key={info.name}>
-                {info.name}
-              </p>
-              <p key={info.types}>
-                {info.types.map(i => {
-                return (
-                  <p key={i}>{i}</p>
-                )
-              })
-              }
-              </p>
-              <p key={info.description}>
-                {info.description}
-              </p>
-            </>
-          )
-        })}
-              </div>
-                
-            </div>
-          )
-        }
+      </div>
+      <div className='card-holder'>
+        <div className='pokemon-image'>
+          <div className={evolution}></div>
+        </div>
+        <div className='pokemon-data'>
+          <div className='stats'>
+            {data?.data.map(info => {
+              return (
+                <>
+                  <p
+                    key={info.data}>
+                    {info.data}
+
+                  </p>
+                  <p key={info.name}>
+                    <span>
+                      Name: 
+                    </span>
+                    <span>
+                      {info.name}
+                    </span>
+                  </p>
+                  <p key={info.types}>
+                    {info.types.map(i => {
+                      return (
+                        <p key={i}>{i}</p>
+                      )
+                    })
+                    }
+                  </p>
+                  <p key={info.description}>
+                    {info.description}
+                  </p>
+                </>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
 
 
 
